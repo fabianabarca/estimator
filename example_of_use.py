1d4f9116287392f993f97b94c120c233fcbf48e3
tonymones <135574584+tonymones@users.noreply.github.com>
2024-01-17 16:15:05 -0600

    Update example_of_use.py

--- example_of_use.py
+++ example_of_use.py
@@ -20,27 +20,7 @@ stops_measurement['arrival_time'] = stops_measurement['arrival_time'].dt.strftim
 # Crear columna 'trip_departure_time'
 stops_measurement['trip_departure_time'] = stops_measurement.groupby('trip_id')['arrival_time'].transform('first')
 
-combinations = stops_measurement[['route_id', 'service_id', 'shape_id', 'stop_id']].drop_duplicates()
-
-# Crear un diccionario para almacenar los polinomios
-polynomials = {}
-
-for i, combination in combinations.iterrows():
-    subset = stops_measurement[(stops_measurement['route_id'] == combination['route_id']) &
-                               (stops_measurement['service_id'] == combination['service_id']) &
-                               (stops_measurement['shape_id'] == combination['shape_id']) &
-                               (stops_measurement['stop_id'] == combination['stop_id'])]
-
-    if not subset.empty:
-        x_values = pd.to_datetime(subset['trip_departure_time'], format='%H:%M:%S').dt.hour * 3600 + \
-                    pd.to_datetime(subset['trip_departure_time'], format='%H:%M:%S').dt.minute * 60 + \
-                    pd.to_datetime(subset['trip_departure_time'], format='%H:%M:%S').dt.second
-
-        degree = 4  # Grado del polinomio, puede ajustarse según las necesidades
-        coefficients = np.polyfit(x_values, subset['delay'], degree)
-        polynomial = np.poly1d(coefficients)
-        polynomials[tuple(combination)] = polynomial
-
+polynomials = est.calcular_polinomios(stops_measurement)
 
 resultado_stop_times = est.generate_stop_times_df()
 # Mostrar el contenido del DataFrame
