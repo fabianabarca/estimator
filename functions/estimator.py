2e4ce10f632d74c55b05dcd091cb78dca3891377
tonymones <antonymonestel@gmail.com>
2024-01-18 12:05:51 -0600

    imports deleted

--- functions/estimator.py
+++ functions/estimator.py
@@ -2,34 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-stops_measurement= pd.read_csv('stop_times_measurement.csv')
-route_stops= pd.read_csv('route_stops.csv')
-trip_times= pd.read_csv('trip_times.csv')
-trips= pd.read_csv('trips.csv')
 
 def calcular_delay(group):
     # Convierte la columna 'arrival_time' en objetos de tiempo
-    group['arrival_time'] = pd.to_datetime(group['arrival_time'], format='%H:%M:%S')
+    group['arrival_time'] = pd.to_datetime(
+        group['arrival_time'], format='%H:%M:%S')
 
     # Índice del primer 'stop_id' donde 'timepoint' es igual a 1
     primer_timepoint_idx = group[group['timepoint'] == 1].index[0]
 
     # Calcula el retraso restando los tiempos de 'arrival_time'
-    group['delay'] = (group['arrival_time'] - 
-    group.loc[primer_timepoint_idx, 'arrival_time']).dt.total_seconds()
+    group['delay'] = (group['arrival_time'] -
+                      group.loc[primer_timepoint_idx, 'arrival_time']).dt.total_seconds()
 
     return group
 
-# Aplicar la función a cada grupo (trip_id, date)
-stops_measurement = stops_measurement.groupby(['trip_id', 'date']).apply(calcular_delay)
-
-# Reiniciar el índice del DataFrame resultante
-stops_measurement.reset_index(drop=True, inplace=True)
-
-stops_measurement['arrival_time'] = stops_measurement['arrival_time'].dt.strftime('%H:%M:%S')
-
-# Crear columna 'trip_departure_time'
-stops_measurement['trip_departure_time'] = stops_measurement.groupby('trip_id')['arrival_time'].transform('first')
 
 def get_sequence_of_stops(route_id, shape_id, route_stops):
     stops_sequence = route_stops[
@@ -39,11 +26,28 @@ def get_sequence_of_stops(route_id, shape_id, route_stops):
 
     # Encontrar todas las paradas para la combinación dada
     sequence_of_stops = stops_sequence['stop_id'].unique()
-    
+
     return sequence_of_stops
-    
+
+
 def calcular_polinomios(stops_measurement):
-    combinations = stops_measurement[['route_id', 'service_id', 'shape_id', 'stop_id']].drop_duplicates()
+
+    # Aplicar la función a cada grupo (trip_id, date)
+    stops_measurement = stops_measurement.groupby(
+        ['trip_id', 'date']).apply(calcular_delay)
+
+    # Reiniciar el índice del DataFrame resultante
+    stops_measurement.reset_index(drop=True, inplace=True)
+
+    stops_measurement['arrival_time'] = stops_measurement['arrival_time'].dt.strftime(
+        '%H:%M:%S')
+
+    # Crear columna 'trip_departure_time'
+    stops_measurement['trip_departure_time'] = stops_measurement.groupby(
+        'trip_id')['arrival_time'].transform('first')
+    
+    combinations = stops_measurement[[
+        'route_id', 'service_id', 'shape_id', 'stop_id']].drop_duplicates()
 
     # Crear un diccionario para almacenar los polinomios
     polynomials = {}
@@ -56,8 +60,9 @@ def calcular_polinomios(stops_measurement):
 
         if not subset.empty:
             x_values = pd.to_datetime(subset['trip_departure_time'], format='%H:%M:%S').dt.hour * 3600 + \
-                        pd.to_datetime(subset['trip_departure_time'], format='%H:%M:%S').dt.minute * 60 + \
-                        pd.to_datetime(subset['trip_departure_time'], format='%H:%M:%S').dt.second
+                pd.to_datetime(subset['trip_departure_time'], format='%H:%M:%S').dt.minute * 60 + \
+                pd.to_datetime(subset['trip_departure_time'],
+                               format='%H:%M:%S').dt.second
 
             degree = 4  # Grado del polinomio, puede ajustarse según las necesidades
             coefficients = np.polyfit(x_values, subset['delay'], degree)
@@ -66,12 +71,12 @@ def calcular_polinomios(stops_measurement):
 
     return polynomials
 
-polynomials = calcular_polinomios(stops_measurement)
 
-def estimator(route_id, service_id, shape_id, start_time, polynomials):
+def estimator(route_id, service_id, shape_id, start_time, polynomials, route_stops):
     # Obtener la secuencia de paradas para la combinación dada
-    sequence_of_stops = get_sequence_of_stops(route_id, shape_id, stops_measurement)
-    
+    sequence_of_stops = get_sequence_of_stops(
+        route_id, shape_id, route_stops)
+
     estimated_arrival_times = {}
     departure_time = pd.to_datetime(start_time, format='%H:%M')
 
@@ -79,27 +84,28 @@ def estimator(route_id, service_id, shape_id, start_time, polynomials):
     for stop_id in sequence_of_stops:
         if (route_id, service_id, shape_id, stop_id) in polynomials:
             # Obtener la función polinomial correspondiente a esta parada
-            polynomial_function = polynomials[(route_id, service_id, shape_id, stop_id)]
+            polynomial_function = polynomials[(
+                route_id, service_id, shape_id, stop_id)]
 
             # Calcular el tiempo de llegada estimado usando la función polinomial y el tiempo de inicio especificado
             x_seconds = departure_time.hour * 3600 + departure_time.minute
             estimated_delay = polynomial_function(x_seconds)
-            
+
             # Calcular el tiempo estimado de llegada sumando el retraso a la hora inicial especificada
-            estimated_arrival = departure_time + pd.Timedelta(seconds=estimated_delay)
-            
+            estimated_arrival = departure_time + \
+                pd.Timedelta(seconds=estimated_delay)
+
             # Guardar el tiempo estimado de llegada para esta parada en el diccionario
-            estimated_arrival_times[stop_id] = estimated_arrival.strftime('%H:%M:%S')
+            estimated_arrival_times[stop_id] = estimated_arrival.strftime(
+                '%H:%M:%S')
         else:
             # No hay datos polinomiales para esta parada
             estimated_arrival_times[stop_id] = "No se puede estimar"
 
     return sequence_of_stops, estimated_arrival_times
-    
-def generate_stop_times_df():
 
-    trip_times= pd.read_csv('trip_times.csv')
-    trips= pd.read_csv('trips.csv')
+
+def generate_stop_times_df(stops_measurement, route_stops, trip_times, trips):
 
     # Crear un DataFrame vacío para acumular los resultados
     stop_times_df = pd.DataFrame(columns=[
@@ -115,13 +121,18 @@ def generate_stop_times_df():
 
         # Reemplazar stops_measurement por trips
         route_id = trips.loc[trips["trip_id"] == trip_id, "route_id"].values[0]
-        service_id = trips.loc[trips["trip_id"] == trip_id, "service_id"].values[0]
+        service_id = trips.loc[trips["trip_id"]
+                               == trip_id, "service_id"].values[0]
         shape_id = trips.loc[trips["trip_id"] == trip_id, "shape_id"].values[0]
 
-        # Llamada a la función estimator
-        sequence_of_stops, estimated_arrival_times = estimator(route_id, service_id, shape_id, start_time, polynomials)
+        polynomials = calcular_polinomios(stops_measurement)
 
-        timepoint_values = [1 if i == 0 else 0 for i in range(len(sequence_of_stops))]
+        # Llamada a la función estimator
+        sequence_of_stops, estimated_arrival_times = estimator(
+            route_id, service_id, shape_id, start_time, polynomials, route_stops)
+
+        timepoint_values = [
+            1 if i == 0 else 0 for i in range(len(sequence_of_stops))]
 
         # Construcción de stop_times_iteration para esta iteración
         stop_times_iteration = pd.DataFrame({
@@ -140,7 +151,6 @@ def generate_stop_times_df():
         })
 
         # Concatenar los resultados de esta iteración al DataFrame principal
-        stop_times_df = pd.concat([stop_times_df, stop_times_iteration], ignore_index=True)
+        stop_times_df = pd.concat(
+            [stop_times_df, stop_times_iteration], ignore_index=True)
     return stop_times_df
-
-
--- functions/example_of_use.py
+++ functions/example_of_use.py
@@ -1,16 +1,17 @@
+import estimator as est
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
-stops_measurement= pd.read_csv('stop_times_measurement.csv')
-route_stops= pd.read_csv('route_stops.csv')
-trip_times= pd.read_csv('trip_times.csv')
-trips= pd.read_csv('trips.csv')
+stops_measurement = pd.read_csv('stop_times_measurement.csv')
+route_stops = pd.read_csv('route_stops.csv')
+trip_times = pd.read_csv('trip_times.csv')
+trips = pd.read_csv('trips.csv')
 
-import estimator as est
 
-resultado_stop_times = est.generate_stop_times_df()
+resultado_stop_times = est.generate_stop_times_df(
+    stops_measurement, route_stops, trip_times, trips)
 # Mostrar el contenido del DataFrame
-print (resultado_stop_times.head(15))
+print(resultado_stop_times.head(15))
